aac4fb1fc4dc2b9dc3fdf6d8d7a02455e4875acf
MaksymYatsun <egocogitio3.14@gmail.com>
2023-07-10 13:36:26 +0300

    Added modal and dropdown

--- app/js/main.js
+++ app/js/main.js
@@ -27,3 +27,38 @@ function toggleMenu() {
 }
 
 hamburger.addEventListener("click", toggleMenu);
+
+const modal = document.getElementById('modal');
+const modalBtn = document.getElementById('modal-btn');
+const closeModal = document.getElementById('close-modal');
+const email = document.getElementById('sign-email');
+const modalErr = document.getElementById('modal-err')
+
+modalBtn.addEventListener('click', () => {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+    modal.style.display = "block";
+    document.body.classList.add("stop-scrolling");
+    modalErr.style.display = "none";
+  } else {
+    modalErr.style.display = "block";
+  }
+});
+
+closeModal.addEventListener('click', () => {
+  modal.style.display = "none";
+  document.body.classList.remove("stop-scrolling")
+})
+
+const dropdown = document.getElementById('dropdown');
+const dropdownBtn = document.getElementById('dropdown-btn');
+
+dropdownBtn.addEventListener('click', (event) => {
+  dropdown.style.display = "block";
+  event.stopPropagation();
+})
+
+document.addEventListener('click', (event) => {
+  if (!event.target.matches('#dropdown')) {
+    dropdown.style.display = "none";
+  }
+})
